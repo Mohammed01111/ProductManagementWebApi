53e39a1f1bd74294c718b24b7d1c4219c7662634
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-12 14:54:35 +0400

    add ApiController

---
+++ ProductManagementWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementWebApi.Services;
+
+namespace ProductManagementWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
