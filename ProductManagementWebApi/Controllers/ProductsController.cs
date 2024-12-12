423c3f5b8b12341bd8f404a396ef3a4d92192deb
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-12 14:56:37 +0400

    add methods to ProductsController

--- ProductManagementWebApi/Controllers/ProductsController.cs
+++ ProductManagementWebApi/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementWebApi.DTO;
 using ProductManagementWebApi.Services;
 
 namespace ProductManagementWebApi.Controllers
@@ -12,6 +13,66 @@ namespace ProductManagementWebApi.Controllers
         public ProductsController(IProductService productService)
         {
             _productService = productService;
+        }
+        [HttpPost]
+        public ActionResult<ProductOutputDto> AddProduct([FromBody] ProductInputDto productDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdProduct = _productService.AddProduct(productDto);
+            return CreatedAtAction(nameof(GetProductById), new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductOutputDto>> GetProducts(
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10)
+        {
+            var products = _productService.GetProducts(pageNumber, pageSize);
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProductOutputDto> GetProductById(int id)
+        {
+            var product = _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<ProductOutputDto> UpdateProduct(
+            int id,
+            [FromBody] ProductInputDto productDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var updatedProduct = _productService.UpdateProduct(id, productDto);
+            if (updatedProduct == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var result = _productService.DeleteProduct(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
         }
     }
 }
