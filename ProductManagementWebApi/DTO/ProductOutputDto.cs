acaf0d28a5fb311fa6e497d9f27b10631473f97c
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-12 14:02:01 +0400

    Product Output Creation

--- ProductManagementWebApi/DTO/ProductOutputDto.cs
+++ ProductManagementWebApi/DTO/ProductOutputDto.cs
@@ -2,5 +2,10 @@
 {
     public class ProductOutputDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
