704501cd6d13d8b5ad876f3d9bf8c637cbfe0103
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-12-12 13:47:14 +0400

    add Product Data Definition

---
+++ ProductManagementWebApi/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementWebApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+    }
+}
